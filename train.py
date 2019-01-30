d4a5658c50f28e9a546640ce44af609f0e20f933
orip <theoripress@gmail.com>
2019-01-30 14:34:54 +0200

    Merge branch 'master' of https://github.com/oripress/hack
    # Conflicts:
    #	train.py

--- train.py
+++ train.py
@@ -97,10 +97,10 @@ def train(args):
             torch.save(discriminator.state_dict(), os.path.join(args.out, 'disc_%d.pkl' % 0))
             gen.eval()
             fixed_fake = gen(fixed_noise).detach().cpu().numpy()
-            fake = batch_data[0].detach().cpu().numpy()
+            real_data = batch_data[0:5].detach().cpu().numpy()
             gen.train()
             display_noise(fixed_fake.squeeze(), os.path.join(args.out, "gen_sample_%d.png" % i))
-            display_noise(fake.squeeze(), os.path.join(args.out, "gen_fake_%d.png" % 0))
+            display_noise(real_data.squeeze(), os.path.join(args.out, "gen_fake_%d.png" % 0))
 
     # for i in tqdm(range(args.iters)):
     #     ### Train Infer ###

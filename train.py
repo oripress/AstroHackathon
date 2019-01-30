1d693ca944f03862521ff38ab464b0173454e0b9
orip <theoripress@gmail.com>
2019-01-30 15:24:21 +0200

    Merge branch 'master' of https://github.com/oripress/hack
    # Conflicts:
    #	train.py

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def train(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--out', default='out')
-    parser.add_argument('--lr', type=float, default=0.002)
+    parser.add_argument('--lr', type=float, default=0.0002)
     parser.add_argument('--bs', type=int, default=32)
     parser.add_argument('--iters', type=int, default=1250000)
     parser.add_argument('--data_path', type=str, required=True)

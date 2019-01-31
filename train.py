d5a310f233c781e2c589bcb0584a642eed9e2dfa
orip <theoripress@gmail.com>
2019-01-31 19:19:33 +0200

    Merge branch 'master' of https://github.com/oripress/hack
    # Conflicts:
    #	train.py

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ def train(args):
             g_optimizer.step()
 
             if total_iters % 5000 == 0:
-                print("Iteration %d >> g_loss: %.4f., d_loss: %.4f." % (i, gen_loss, d_loss))
+                print("Iteration %d >> g_loss: %.4f., d_loss: %.4f." % (total_iters, gen_loss, d_loss))
                 torch.save(gen.state_dict(), os.path.join(args.out, 'gen_%d.pkl' % 0))
                 torch.save(discriminator.state_dict(), os.path.join(args.out, 'disc_%d.pkl' % 0))
                 gen.eval()
